7cb983ea4dee8dd7464845516005d828f75cc4bb
swapnil chauhan <selnox93@gmail.com>
2024-01-15 14:52:21 +0530

    sfgdfg

--- src/Components/pages/RSS/Rss.jsx
+++ src/Components/pages/RSS/Rss.jsx
@@ -7,22 +7,22 @@ export default function Rss() {
             <div className='col-12 center'>
                 <ol className='RSS' >
                     <li>
-                        <a href='https://g11prediction.com/Rss/Breakingnewsrss-Feed.xml' className='calling'><span> <FaRssSquare /> Breaking News </span> </a>
+                        <a href='https://g11prediction.com/RSS/Breakingnewsrss-Feed.xml' className='calling'><span> <FaRssSquare /> Breaking News </span> </a>
                     </li>
                     <li>
-                        <a href='https://g11prediction.com/Rss/icc-cricket-world-cup-2023RSS-feed.xml' className='calling'><span> <FaRssSquare />Icc-cricket-world-cup-2023 </span> </a>
+                        <a href='https://g11prediction.com/RSS/icc-cricket-world-cup-2023RSS-feed.xml' className='calling'><span> <FaRssSquare />Icc-cricket-world-cup-2023 </span> </a>
 
                     </li>
                     <li>
-                        <a href='https://g11prediction.com/Rss/icc-cricket-world-cup-2024RSS-feed.xml' className='calling'><span> <FaRssSquare /> Icc-cricket-world-cup-2024</span> </a>
+                        <a href='https://g11prediction.com/RSS/icc-cricket-world-cup-2024RSS-feed.xml' className='calling'><span> <FaRssSquare /> Icc-cricket-world-cup-2024</span> </a>
 
                     </li>
                     <li>
-                        <a href='https://g11prediction.com/Rss/ipl-2023RSS-feed.xml' className='calling'><span> <FaRssSquare />Ipl-2023 </span> </a>
+                        <a href='https://g11prediction.com/RSS/ipl-2023RSS-feed.xml' className='calling'><span> <FaRssSquare />Ipl-2023 </span> </a>
 
                     </li>
                     <li>
-                        <a href='https://g11prediction.com/Rss/ipl-2024RSS-feed.xml' className='calling'><span> <FaRssSquare />Ipl-2024 </span> </a>
+                        <a href='https://g11prediction.com/RSS/ipl-2024RSS-feed.xml' className='calling'><span> <FaRssSquare />Ipl-2024 </span> </a>
                     </li>
                 </ol>
             </div>
